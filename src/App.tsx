5145552cd63a4b92a21ac5864b110783f4e6c2ba
Rahul P <rahul.parmar@team.exelanceit.in>
2025-05-05 16:57:58 +0530

    update tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import AppRoutes from "./routes/AppRoutes";
 import Login from "@pages/auth/Login";
 
 function App() {
-  // return <AppRoutes />;
   return (
     <>
       <BrowserRouter>
--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>
-      <h3>ABCDEFGHIJKLMNOPQRSTUVWXYZ</h3>
-      <h3>abcdefghijklmnopqrstuvwxyz</h3>
+    <div className='bg-slate-400 container flex-col max-w-full'>
+      <h3 className='text-3xl font-bold'>ABCDEFGHIJKLMNOPQRSTUVWXYZ</h3>
+      <h3 className='text-xl font-medium'>abcdefghijklmnopqrstuvwxyz</h3>
     </div>
   )
 }
