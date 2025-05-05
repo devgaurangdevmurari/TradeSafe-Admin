3f7e8dcf2bcd66a87774f5dc0ec333b1f31500c2
Rahul P <rahul.parmar@team.exelanceit.in>
2025-05-05 14:01:46 +0530

    configured tailwind css & custom fonts

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Login = () => {
   return (
     <div>
-      <h1>LOGINIINNn</h1>
+      <h3>ABCDEFGHIJKLMNOPQRSTUVWXYZ</h3>
+      <h3>abcdefghijklmnopqrstuvwxyz</h3>
     </div>
   )
 }
